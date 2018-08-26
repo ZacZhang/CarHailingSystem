439bbcaa76136acaa952dd263f94621c7c7f7e54
zaczhang <zaczhang19@gmail.com>
2018-08-26 00:28:41 -0400

    implement zuul-service

--- zuul-service/src/main/java/com/zac/zuulservice/AccessTokenFilter.java
+++ zuul-service/src/main/java/com/zac/zuulservice/AccessTokenFilter.java
@@ -1,4 +1,51 @@
 package com.zac.zuulservice;
 
-public class AccessTokenFilter {
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class AccessTokenFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessTokenFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+
+        return ctx.getRequest().getRequestURI().contains("dispatch");
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            log.warn("access token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseBody("401");
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("access token ok");
+        return null;
+    }
 }
