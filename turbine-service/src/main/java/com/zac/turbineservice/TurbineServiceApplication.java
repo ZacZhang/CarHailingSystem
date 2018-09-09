331eec4f8cacb52b1b7ed9010d151ba8f159ee2a
zaczhang <zaczhang19@gmail.com>
2018-09-08 20:58:17 -0400

    implement turbine-service

--- turbine-service/src/main/java/com/zac/turbineservice/TurbineServiceApplication.java
+++ turbine-service/src/main/java/com/zac/turbineservice/TurbineServiceApplication.java
@@ -2,7 +2,13 @@ package com.zac.turbineservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableEurekaClient
 @SpringBootApplication
 public class TurbineServiceApplication {
 
