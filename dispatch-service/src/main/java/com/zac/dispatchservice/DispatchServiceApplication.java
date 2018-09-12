fab3bd297f6a77182e3f48f21e2714711180b42b
zaczhang <zaczhang19@gmail.com>
2018-09-11 23:46:58 -0400

    fix bug for dispatch-service/

--- dispatch-service/src/main/java/com/zac/dispatchservice/DispatchServiceApplication.java
+++ dispatch-service/src/main/java/com/zac/dispatchservice/DispatchServiceApplication.java
@@ -2,6 +2,7 @@ package com.zac.dispatchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableHystrix
 @EnableFeignClients(basePackageClasses = LocationServiceFeignClient.class)
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DispatchServiceApplication {
 
     public static void main(String[] args) {
