8a6ab1aa1e9f33f516fbff63184cf2e13b703d13
zaczhang <zaczhang19@gmail.com>
2018-09-15 15:30:54 -0400

    update postgresql

--- postgresql/createDrivers.sql
+++ postgresql/createDrivers.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS Drivers;
 
 CREATE TABLE Drivers (
-	did integer,
-	firstName varchar(40),
-	lastName varchar(40),
+	id bigint,
+	first_name varchar(40),
+	last_name varchar(40),
 	address varchar(200),
 	phone varchar(20),
-	createdAt timestamp,
-	isActive boolean,
-	PRIMARY KEY(did)
+	created_at timestamp,
+	is_active boolean,
+	PRIMARY KEY(id)
 )
--- postgresql/createTrips.sql
+++ postgresql/createTrips.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS Trips;
 
 CREATE TABLE Trips (
-	tid integer,
-	driverId integer REFERENCES drivers (did),
-	riderId integer REFERENCES riders (rid),
+	id long,
+	driverId integer REFERENCES drivers (id),
+	riderId integer REFERENCES riders (id),
 	origin varchar(40),
 	destination varchar(40),
 	status integer,
 	createdAt timestamp,
-	PRIMARY KEY(tid)
+	PRIMARY KEY(id)
 )
