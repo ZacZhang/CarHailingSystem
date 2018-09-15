8f6ea5f3b63e7041050fc5c6c29cbd9f12774045
zaczhang <zaczhang19@gmail.com>
2018-09-14 23:38:26 -0400

    update code for dispatchservice

--- dispatch-service/src/main/java/com/zac/dispatchservice/DispatchServiceApplication.java
+++ dispatch-service/src/main/java/com/zac/dispatchservice/DispatchServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableHystrixDashboard
 @EnableHystrix
