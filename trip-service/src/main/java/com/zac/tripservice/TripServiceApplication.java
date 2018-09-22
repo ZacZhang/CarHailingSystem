092cc243e210dee7c81d1009d1db320901a5f84c
zaczhang <zaczhang19@gmail.com>
2018-09-22 12:16:57 -0400

    add trip-service

---
+++ trip-service/src/main/java/com/zac/tripservice/TripServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zac.tripservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripServiceApplication.class, args);
+    }
+}
