e1a81735e79b27d0c0b9b8554a5ac8740a71a20d
zaczhang <zaczhang19@gmail.com>
2018-09-22 12:02:26 -0400

    implement config-server

--- config-server/src/main/java/com/zac/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/zac/configserver/ConfigServerApplication.java
@@ -2,7 +2,11 @@ package com.zac.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
