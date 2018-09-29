6599979eb6d544fd2d5213e222ab0b463ab2d094
zaczhang <zaczhang19@gmail.com>
2018-09-29 12:26:54 -0400

    create rider-client

---
+++ rider-client/src/main/java/com/zac/riderclient/RiderClientApplication.java
@@ -0,0 +1,12 @@
+package com.zac.riderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiderClientApplication.class, args);
+    }
+}
