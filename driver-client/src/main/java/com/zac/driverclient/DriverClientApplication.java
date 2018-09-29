dfe01fad8a230050f77c46eb6789c2c042d6c925
zaczhang <zaczhang19@gmail.com>
2018-09-29 00:13:30 -0400

    add driver-client

---
+++ driver-client/src/main/java/com/zac/driverclient/DriverClientApplication.java
@@ -0,0 +1,12 @@
+package com.zac.driverclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverClientApplication.class, args);
+    }
+}
